dc3570a764cdd521381469414f6b2270b85da2c9
Mik Freedman <mfreedman@vmware.com>
2021-02-16 13:22:48 -0500

    remove vim, cleanup linux bootstrap

--- bootstrap-linux.sh
+++ bootstrap-linux.sh
@@ -3,12 +3,10 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get --assume-yes install tmux git zsh silversearcher-ag net-tools unattended-upgrades software-properties-common python-software-properties
-sudo add-apt-repository -yu ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get --assume-yes install neovim
 
-chsh -s /usr/bin/zsh joe
-git clone https://github.com/joefitzgerald/dotfiles.git
+chsh -s /usr/bin/zsh $USER
+git clone https://github.com/mikfreedman/dotfiles.git
 export GOVERSION="1.11.2"
 if [[ "$OSTYPE" == darwin* ]]; then
   wget -O go.tar.gz "https://storage.googleapis.com/golang/go$GOVERSION.darwin-amd64.tar.gz"
@@ -17,14 +15,11 @@ else
 fi
 sudo rm -rf /usr/local/go
 sudo mkdir -p /usr/local/go
-sudo chown joe:joe /usr/local/go
+sudo chown $USER:$USER /usr/local/go
 tar -C /usr/local -xzf go.tar.gz
 rm -rf go.tar.gz
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-rm -rf ~/.vim/plugged
 rm -rf ~/.zprezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
-ln -fs "$HOME/dotfiles/vimrc" "$HOME/.vimrc"
 ln -fs "$HOME/dotfiles/zpreztorc-linux" "$HOME/.zpreztorc"
 ln -fs "$HOME/dotfiles/zprofile" "$HOME/.zprofile"
 ln -fs "$HOME/dotfiles/zshrc" "$HOME/.zshrc"
@@ -32,6 +27,6 @@ ln -fs "$HOME/dotfiles/zlogin" "$HOME/.zlogin"
 ln -fs "$HOME/dotfiles/zlogout" "$HOME/.zlogout"
 ln -fs "$HOME/dotfiles/zshenv" "$HOME/.zshenv"
 mkdir -p "$HOME/go"
-git config --global user.email "jfitzgerald@pivotal.io"
-git config --global user.name "Joe Fitzgerald"
+git config --global user.email "github@michael-freedman.compopt"
+git config --global user.name "Mik Freedman"
 git config --global pull.rebase true
